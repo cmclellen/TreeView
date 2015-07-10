a2ad151cb9318bf6aa79ffa4114f4a588f9737f9
Craig McLellen <cmclellen@gmail.com>
2015-07-11 05:39:54 +1000

    Initial commit

---
+++ CompanyABC.TreeView.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CompanyABC.TreeView.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
