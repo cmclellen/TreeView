5e46994da4d8b2869931953068bbd1fbf74d71ab
Craig McLellen <cmclellen@gmail.com>
2015-07-12 11:07:24 +1000

    Updated documentation

--- CompanyABC.TreeView.Web/dist/partials/views/about.js
+++ CompanyABC.TreeView.Web/dist/partials/views/about.js
@@ -16,18 +16,8 @@ module.run(['$templateCache', function($templateCache) {
     '        <li>\n' +
     '            <h3>Prerequisites to run the application</h3>\n' +
     '            <ul>\n' +
-    '                <li>Visual Studio 2012/2013/2015 (I haven\'t tested it in VS 2013 or 2015, as I haven\'t downloaded the express versions to my home machine yet as I\'ve been playing more with NodeJs modules, but I\'m almost sure it\'ll be fine).</li>\n' +
-    '                <li>To have the .js tests running in test explorer, you\'ll need to install the <a href="https://visualstudiogallery.msdn.microsoft.com/f8741f04-bae4-4900-81c7-7c9bfb9ed1fe">Chutzpah Test Adapter for the Test Explorer</a> and optionally the <a href="https://visualstudiogallery.msdn.microsoft.com/71a4e9bd-f660-448f-bd92-f5a65d39b7f0">Chutzpah Test Runner Context Menu Extension</a> extensions for Visual Studio.</li>\n' +
-    '                <li>\n' +
-    'I\'m using NodeJs & Gulp to generate the template-cache js files for my angular views, but I\'ve kept it \n' +
-    'separate from the VS build process ("AfterBuild" step in the CompanyABC.TreeView.Web.csproj) to avoid \n' +
-    'you having to install NodeJs on your local machine, but if this was an actual project, I would have \n' +
-    'included it in my build process instead as this is not the only benefit of using a task runner like Gulp and I would have\n' +
-    'not included the generated associated template-cache js files in my source control (as these can be generated from files \n' +
-    'that are already in my source control). I use gulp to minify/uglify my .js, .css, .html files, as well as running preprocessors for .less, es6 files, as well\n' +
-    'as "stylecop" type tasks such as eslint. VS 2015 actually natively caters for these types of task runners.\n' +
-    '\n' +
-    '                </li>\n' +
+    '                <li>Visual Studio 2012/2013/2015 (I haven\'t tested it in VS 2013 or 2015, as I haven\'t downloaded the express versions to my home machine yet as I\'ve been dabbling more with NodeJs lately, but I\'m almost sure it\'ll be fine).</li>\n' +
+    '                <li>To have the .js tests running in Visual Studio\'s "Test Explorer", you\'ll need to install the <a href="https://visualstudiogallery.msdn.microsoft.com/f8741f04-bae4-4900-81c7-7c9bfb9ed1fe">Chutzpah Test Adapter for the Test Explorer</a> and optionally the <a href="https://visualstudiogallery.msdn.microsoft.com/71a4e9bd-f660-448f-bd92-f5a65d39b7f0">Chutzpah Test Runner Context Menu Extension</a> extensions for Visual Studio.</li>\n' +
     '            </ul>\n' +
     '        </li>\n' +
     '        <li>\n' +
@@ -40,6 +30,30 @@ module.run(['$templateCache', function($templateCache) {
     '            <p>\n' +
     'When the application is running, you can access a tree of depth 2 by either typing into the internet browser address bar or clicking on the url <a href="{{uiState.sampleTreeViewUrl}}" ng-bind="uiState.sampleTreeViewUrl"></a>.\n' +
     '            </p>\n' +
+    '        </li>\n' +
+    '        <li>\n' +
+    '            <h3>Notes</h3>\n' +
+    '            <p>\n' +
+    'For the DAL, if it were tied to an actual DB, I usually have an accompanying "IntegrationTests" project\n' +
+    'to ensure that the integration between the DAL and actual DB is correct. In the DAL layer, I \n' +
+    'would usually use an ORM such as EntityFramework or Dapper - there are usually performance issues \n' +
+    'related to bulk data manipulation or complex queries with ORMs, but there are solutions for these unique\n' +
+    'situations which cater for these specific scenarios. </p>\n' +
+    '            <p>\n' +
+    'In this project I\'ve clumped both the WebApi and the Web into one project, but if this was an actual project, \n' +
+    'I would have separated these out into separate projects as the WebApi is reusable by other clients \n' +
+    '(e.g. mobile apps, etc.).\n' +
+    '            </p>\n' +
+    '            <p>\n' +
+    'I\'m using NodeJs & Gulp to generate the template-cache js files for my angular views, but I\'ve kept it \n' +
+    'separate from the VS build process ("AfterBuild" step in the CompanyABC.TreeView.Web.csproj) to avoid \n' +
+    'you having to install NodeJs on your local machine, but if this was an actual project, I would have \n' +
+    'included it in my build process instead as this is not the only benefit of using a task runner like Gulp and I would have\n' +
+    'not included the generated associated template-cache js files in my source control (as these can be generated from files \n' +
+    'that are already in my source control). I use gulp to minify/uglify my .js, .css, .html files, as well as running preprocessors for .less, es6 files, as well\n' +
+    'as "stylecop" type tasks such as eslint. VS 2015 actually natively caters for these types of task runners.\n' +
+    '\n' +
+    '                </p>\n' +
     '        </li>\n' +
     '        <li>\n' +
     '            <h3>Troubleshooting</h3>\n' +
